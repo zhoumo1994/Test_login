9b905f9015046910332da45cd1b52fe8ce6b84a4
zhoumo09 <1254259123@qq.com>
2019-04-10 09:54:32 +0800

    Initial commit

---
+++ Testcase/data_time.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome()
+driver.implicitly_wait(5)
+driver.maximize_window()
+driver.get('https://admin.91duobaoyu.com/admin_new/#/login')
+sw = driver.find_element_by_class_name('el-input__inner')
+sw.clear()
+sw.send_keys('zhoumo')
+sd = driver.find_element_by_name('password')
+sd.clear()
+sd.send_keys('402496')
+driver.find_element_by_xpath('//*[@id="app"]/div/form/button[1]').click()
+
+def ssr(self):
+    md = driver.find_elements_by_xpath('//*[@id="app"]/div/div[2]/ul/div[3]/button/span/a')
+    data = md.get_text()
+    self.assertEqual('问题反馈', data)
+
+driver.close()
